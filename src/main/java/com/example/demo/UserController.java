1578b8f07a4300d8192787be2fb7b26dfe8883d1
Alexander Kh <khidiralievalexander@gmail.com>
2026-09-12 20:54:39 +0300

    Removed unnecessary dependency.

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -3,7 +3,6 @@ package com.example.demo;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
